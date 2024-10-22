8f0b3f7027a94f1cb982b334611163814a9ea919
Anas Asimi <anas2020cr7@gmail.com>
2024-10-22 22:21:38 +0100

    move server launching to server.js

--- api/config/connect.db.js
+++ api/config/connect.db.js
@@ -1,26 +1,17 @@
 import mongoose from "mongoose";
-import express from "express";
 
 /**
  * connects to MongoDB using mongoose
  * @param {string} uri - MongoDB connection string
- * @param {express.Express} app - Express server
- *    app server started only after successful db connection
- * @param {number} app_port - Express api port for app
  */
-const connectMongoDB = (uri, app, app_port) => {
-  mongoose
-    .connect(uri)
-    .then(() => {
-      console.log("Successfully connected to db");
-
-      app.listen(app_port, () => {
-        console.log(`Server is listening on port ${app_port}`);
-      });
-    })
-    .catch((error) => {
-      console.error(`db connection error: ${error.message}`);
-    });
+async function connectMongoDB(uri, app, app_port) {
+  try {
+    await mongoose
+      .connect(uri);
+    console.log("Successfully connected to db");
+  } catch (error) {
+    console.error(`db connection error: ${error.message}`);
+  }
 };
 
 export default connectMongoDB;
--- api/server.js
+++ api/server.js
@@ -1,16 +1,23 @@
-import { config } from "dotenv";
-import { fileURLToPath } from "url";
-import path, { dirname } from "path";
-import app from "./app.js";
 import connectMongoDB from "./config/connect.db.js";
-import { resolveCurrentPath } from "./lib/utils.js";
+import app from "./app.js";
 
-const __dirname = resolveCurrentPath(import.meta.url);
-config({
-  path: path.join(__dirname, "..", "api", ".env"),
-});
+// ? i dont think this is necessary at least for now.
+// import { config } from "dotenv";
+// import { fileURLToPath } from "url";
+// import path, { dirname } from "path";
+// import { resolveCurrentPath } from "./lib/utils.js";
+// const __dirname = resolveCurrentPath(import.meta.url);
+// config({
+//   path: path.join(__dirname, "..", "api", ".env"),
+// });
 
 const DB_URI = process.env.DB_URI || "mongodb://127.0.0.1:27017/TalentBridge";
 const API_PORT = process.env.API_PORT || 3000;
 
-connectMongoDB(DB_URI, app, API_PORT);
+// connecting to mongoDB
+connectMongoDB(DB_URI).then(() => {
+  // starting the express server
+  app.listen(API_PORT, () => {
+    console.log(`Server is listening on port ${API_PORT}`);
+  });
+})
