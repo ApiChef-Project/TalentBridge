ba3061d7b9f83779cddcaf6f191b95ea7cb517de
Anas Asimi <anas2020cr7@gmail.com>
2024-10-22 22:11:11 +0100

    make drop.dp.js importable + improve readabilty

--- api/config/drop.db.js
+++ api/config/drop.db.js
@@ -2,19 +2,29 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-if (process.env.ENVIRONMENT === "dev") {
-  const dropDatabase = async () => {
-    try {
-      const DB_URI = "mongodb://127.0.0.1:27017/TalentBridge";
-      const client = await mongoose.connect(DB_URI);
-      await client.connection.dropDatabase();
-      console.log("Database dropped successfully");
-    } catch (error) {
-      console.log(`Error While Dropping DB: ${error.message}`);
-    } finally {
-      mongoose.connection.close();
-    }
-  };
+const DB_URI = process.env.DB_URI || "mongodb://127.0.0.1:27017/TalentBridge";
 
-  dropDatabase();
+/**
+ * drops a MongoDB database
+ * @param {string} url - MongoDB connection string
+ */
+async function dropDatabase(url) {
+  try {
+    const client = await mongoose.connect(url);
+    await client.connection.dropDatabase();
+    console.log("Database dropped successfully");
+  } catch (error) {
+    console.log(`Error While Dropping DB: ${error.message}`);
+  } finally {
+    mongoose.connection.close();
+  }
+};
+
+// When the module is run as a standalone script.
+if (import.meta.url === new URL('', import.meta.url).href) {
+  if (process.env.ENVIRONMENT === "dev") {
+    dropDatabase(DB_URI);
+  }
 }
+
+export default dropDatabase
