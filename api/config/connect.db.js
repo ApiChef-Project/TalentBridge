f0d4c4073cdf148ea4a0d198e4adcb6915668f1b
Anas Asimi <anas2020cr7@gmail.com>
2024-10-23 21:48:19 +0100

    add 'verbouse' arg to connectMongoDB function

--- api/config/connect.db.js
+++ api/config/connect.db.js
@@ -3,15 +3,17 @@ import mongoose from "mongoose";
 /**
  * connects to MongoDB using mongoose
  * @param {string} uri - MongoDB connection string
+ * @param {boolean} verbouse - to control logging
  */
-async function connectMongoDB(uri, app, app_port) {
-  try {
-    await mongoose
-      .connect(uri);
-    console.log("Successfully connected to db");
-  } catch (error) {
-    console.error(`db connection error: ${error.message}`);
-  }
-};
+async function connectMongoDB(uri, verbouse = true) {
+	try {
+		await mongoose.connect(uri);
+		if (verbouse) {
+			console.log("Successfully connected to db");
+		}
+	} catch (error) {
+		console.error(`db connection error: ${error.message}`);
+	}
+}
 
 export default connectMongoDB;
