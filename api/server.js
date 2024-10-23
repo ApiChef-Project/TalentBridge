3f106dc5d54e0d7c0ded24e4799ad48cdaa48c00
Anas Asimi <anas2020cr7@gmail.com>
2024-10-23 13:26:03 +0100

    use dotenv.config to initialize .env keys

--- api/server.js
+++ api/server.js
@@ -1,16 +1,14 @@
 import connectMongoDB from "./config/connect.db.js";
+import { config } from "dotenv";
 import app from "./app.js";
 
 // ? i dont think this is necessary at least for now.
-// import { config } from "dotenv";
 // import { fileURLToPath } from "url";
 // import path, { dirname } from "path";
 // import { resolveCurrentPath } from "./lib/utils.js";
 // const __dirname = resolveCurrentPath(import.meta.url);
-// config({
-//   path: path.join(__dirname, "..", "api", ".env"),
-// });
 
+config();
 const DB_URI = process.env.DB_URI || "mongodb://127.0.0.1:27017/TalentBridge";
 const API_PORT = process.env.API_PORT || 3000;
 
@@ -18,6 +16,6 @@ const API_PORT = process.env.API_PORT || 3000;
 connectMongoDB(DB_URI).then(() => {
   // starting the express server
   app.listen(API_PORT, () => {
-    console.log(`Server is listening on port ${API_PORT}`);
+    console.log(`Server is listening on http://localhost:${API_PORT}`);
   });
 })
