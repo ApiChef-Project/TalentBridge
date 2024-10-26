9dca7ec4567fe315e33dd30a8c06f1fd87f8907e
Anas Asimi <anas2020cr7@gmail.com>
2024-10-26 16:26:18 +0100

    create a file to store mock data

---
+++ api/tests/mockData.js
@@ -0,0 +1,14 @@
+export const user1payload = {
+	firstName: "anas",
+	lastName: "asimi",
+	email: "anas2020cr7@gmail.com",
+	phone: "0123456789",
+	password: "anas123",
+};
+export const user2payload = {
+	firstName: "anas2",
+	lastName: "asimi2",
+	email: "anas3030@gmail.com",
+	phone: "00000000",
+	password: "anas456",
+};
